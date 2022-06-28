12518f9092ae3da0603a0fc0c453250d7a9029b6
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-06-28 10:54:03 -0300

    frontend started

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className="h-full">
+      <div className="bg-zinc-800 w-80 h-full">
+        <h1>APP</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
